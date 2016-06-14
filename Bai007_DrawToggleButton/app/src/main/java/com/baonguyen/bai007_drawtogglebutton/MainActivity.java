90ab0c463261fa57d124503c4ccc7e7b7d1bbd9b
Nguyễn Tuấn Bảo <nguyentuanbao93@gmail.com>
2016-06-14 14:42:08 +0700

    Vẽ hiệu ứng thay đổi cho 1 toggleButton

---
+++ Bai007_DrawToggleButton/app/src/main/java/com/baonguyen/bai007_drawtogglebutton/MainActivity.java
@@ -0,0 +1,13 @@
+package com.baonguyen.bai007_drawtogglebutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
